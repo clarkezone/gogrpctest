760bd6452caf0290ee1a5e7f29dddae3a0208623
James Clarke <james@clarkezone.net>
2018-12-31 19:52:56 +0000

    fix install script

--- getprotoc.sh
+++ getprotoc.sh
@@ -1,5 +1,6 @@
 curl -L -O https://github.com/protocolbuffers/protobuf/releases/download/v3.6.1/protoc-3.6.1-linux-x86_64.zip
 unzip protoc-3.6.1-linux-x86_64.zip -d protoc
 rm protoc-3.6.1-linux-x86_64.zip
-chmod 700 bin/protoc
+chmod 700 protoc/bin/protoc
+go get -u github.com/golang/protobuf/protoc-gen-go
 
